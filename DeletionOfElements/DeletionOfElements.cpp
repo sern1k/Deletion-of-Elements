544a4e074f2c4b687c0c0e70e3e6d125e3701793
Pola Madej <66171277+sern1k@users.noreply.github.com>
2021-05-25 17:28:24 +0200

    Addition of comments

--- DeletionOfElements/DeletionOfElements.cpp
+++ DeletionOfElements/DeletionOfElements.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void PrintDeque(deque<string> pets)
+void PrintDeque(deque<string> pets) //method responsible for printing deque
 {
     cout << "Deque Pets: ";
     for (int i = 0; i < pets.size(); i++)
@@ -14,7 +14,7 @@ void PrintDeque(deque<string> pets)
     cout << "\n\n";
 }
 
-void PrintVector(vector<int> num)
+void PrintVector(vector<int> num)   //method responsible for printing vector
 {
     cout << "Vector Numbers: ";
     for (int i = 0; i < num.size(); i++)
@@ -26,18 +26,18 @@ void PrintVector(vector<int> num)
 
 int main()
 {
-    deque<string> pets{ "parrot", "bunny", "dog", "cat"};
+    deque<string> pets{ "parrot", "bunny", "dog", "cat"};   //random deque
     PrintDeque(pets);
 
-    pets.pop_back();
+    pets.pop_back();    //removal of an item from the back
     PrintDeque(pets);
 
-    pets.pop_front();
+    pets.pop_front();   //removal of an item from the front
     PrintDeque(pets);
 
-    vector<int> num{ 9,5,2,6,3,8,9,2,4 };
+    vector<int> num{ 9,5,2,6,3,8,9,2,4 };   //random vector
     PrintVector(num);
 
-    num.pop_back();
+    num.pop_back(); //removal of an item
     PrintVector(num);
 }
